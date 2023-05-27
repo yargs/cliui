73b9dd4ac2dbe99d74c7c3cf93fe6d9a57e93b9c
John Gee <john@ruru.gen.nz>
2023-05-27 14:41:00 +1200

    feat: add default width to Deno implementation

--- deno.ts
+++ deno.ts
@@ -1,10 +1,19 @@
+/* global Deno */
+
 // Bootstrap cliui with CommonJS dependencies:
 import { cliui, UI } from './build/lib/index.js'
 import type { UIOptions } from './build/lib/index.d.ts'
 import { wrap, stripAnsi } from './build/lib/string-utils.js'
 
 export default function ui (opts: UIOptions): UI {
-  return cliui(opts, {
+  const optsWithWidth = opts ?? {}
+  if (!optsWithWidth.width) {
+    const { columns } = Deno.consoleSize()
+    if (columns) {
+      optsWithWidth.width = columns
+    }
+  }
+  return cliui(optsWithWidth, {
     stringWidth: (str: string) => {
       return [...str].length
     },
