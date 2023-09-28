c9b773d0c09530fdf04c7fdd67889b49d92e122b
Craigory Coppola <craigorycoppola@gmail.com>
2023-09-28 12:06:34 -0400

    fix: update typings to accept zero arguments
    The default export for cliui is a function that takes 1 or 0 arguments, as indicated in the examples in the readme. This updates the typescript typings so that passing zero arguments is valid

--- deno.ts
+++ deno.ts
@@ -3,7 +3,7 @@ import { cliui, UI } from './build/lib/index.js'
 import type { UIOptions } from './build/lib/index.d.ts'
 import { wrap, stripAnsi } from './build/lib/string-utils.js'
 
-export default function ui (opts: UIOptions): UI {
+export default function ui (opts?: UIOptions): UI {
   return cliui(opts, {
     stringWidth: (str: string) => {
       return [...str].length
