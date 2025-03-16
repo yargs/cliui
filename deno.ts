5a521de7ea88f262236394c8d96775bcf50ff0a4
John Gee <john@ruru.gen.nz>
2025-03-17 03:41:23 +1300

    feat!: cliui is now ESM only (#165)

--- deno.ts
+++ deno.ts
@@ -1,14 +1,14 @@
 // Bootstrap cliui with CommonJS dependencies:
 import { cliui, UI } from './build/lib/index.js'
 import type { UIOptions } from './build/lib/index.d.ts'
-import { wrap, stripAnsi } from './build/lib/string-utils.js'
+import stringWidth from 'string-width'
+import stripAnsi from 'strip-ansi'
+import wrapAnsi from 'wrap-ansi'
 
 export default function ui (opts: UIOptions): UI {
   return cliui(opts, {
-    stringWidth: (str: string) => {
-      return [...str].length
-    },
+    stringWidth,
     stripAnsi,
-    wrap
+    wrap: wrapAnsi
   })
 }
